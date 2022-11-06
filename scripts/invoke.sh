e236619f8efcaa85d5896d4a002837af25df4dd1
toby moreno <tobymoreno@Karols-Air.home>
2022-11-06 11:15:03 -0800

    fixed the loopback test

--- scripts/invoke.sh
+++ scripts/invoke.sh
@@ -4,3 +4,4 @@ curl -k --cert identity.pem --cacert truststore.pem https://localhost:8444/api/h
 curl -k --cert identity.pem --cacert truststore.pem --pass secret -H 'Content-Type: application/json' -H 'Accept: application/json' -d @data.json https://localhost:8444/api/nse  | /c/Users/tmoreno/Downloads/jqwin/jq
 
    
+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
