5c3acebdd7d0b4038545bcfbd8d1342e685bb9d0
toby moreno <tobymoreno@Karols-Air.home>
2022-11-08 18:19:04 -0800

    removed keys

--- scripts/invoke.sh
+++ scripts/invoke.sh
@@ -4,11 +4,7 @@ curl -k --cert identity.pem --cacert truststore.pem https://localhost:8444/api/h
 curl -k --cert identity.pem --cacert truststore.pem --pass secret -H 'Content-Type: application/json' -H 'Accept: application/json' -d @data.json https://localhost:8444/api/nse  | /c/Users/tmoreno/Downloads/jqwin/jq
 
    
-#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
 
-#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
-
-#xxxx_pat_11AFATXFQ0BwSycmVUg6wN_9NK2lgpu0cQclVBDnG4sbI0HGAKuCPemncQUJdrpbrvEJP7YBSDC0***sss
 
 
 Z2l0aHViX3BhdF8xMUFGQVRYRlEwQndTeWNtVlVnNndOXzlOSzJsZ3B1MGNRY2xWQkRuRzRzYkkwSEdBS3VDUGVtbmNRVUpkcnBicnZFSlA3WUJTREMwdEZWOGxkCg==
