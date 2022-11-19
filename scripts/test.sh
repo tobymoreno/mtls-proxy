7526f7a459ddba7cb4c7916e4e710764b1df1524
toby moreno <tobymoreno@Karols-Air.home>
2022-11-19 14:51:16 -0800

    adding version 6.9.4

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,19 +3,20 @@
 IMAGE_CHECK="Image is up to date"
 
 gcloud container clusters get-credentials autopilot-cluster-1 --zone us-west3
+docker login -utoby.moreno@gmail.com devsecopsenv.jfrog.io -p "${jfrog_pwd}"
+
+kubectl apply -f ../secrets.yml
+kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "acr-auth"}]}' -n default
 
 while [ 1 -ne 0 ]
 do
     date
-    docker login -utoby.moreno@gmail.com devsecopsenv.jfrog.io -p "${jfrog_pwd}"
     docker pull devsecopsenv.jfrog.io/default-docker/1rom2ny/mtls-proxy:v7 | grep "${IMAGE_CHECK}"
 
     if [ $? -ne 1 ]; then 
         echo "${IMAGE_CHECK}"
     else
         echo "There is a new image. Update the service"
-        kubectl apply -f ../secrets.yml
-        kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "acr-auth"}]}' -n default
         kubectl replace -f ../deploymentservice-cluster.yml
         sleep 2
         kubectl apply -f ../mtls-loadbalancer.yaml
