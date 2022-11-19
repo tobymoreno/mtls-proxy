84aed871a841b6050e5815384f2b786d0171e6fc
toby moreno <tobymoreno@Karols-Air.home>
2022-11-18 19:49:46 -0800

    adding 6.8.7

--- src/main/java/mil/navy/mtls/proxy/WebHandlerInterceptor.java
+++ src/main/java/mil/navy/mtls/proxy/WebHandlerInterceptor.java
@@ -37,8 +37,8 @@ public class WebHandlerInterceptor implements HandlerInterceptor {
         if (servletPath != null && servletPath.startsWith("/error")) {
 //            throw new Exception("Internal Error");
         } else {
-            System.out.println("I'm processing version 6.8.6 in EKS");
-            String[] response = new String[]{"EKS Loopback Test Version 6.8.6"};
+            System.out.println("I'm processing version 6.8.7 in EKS");
+            String[] response = new String[]{"EKS Loopback Test Version 6.8.7"};
             responseServlet.getWriter().write(JSON.getInstance().stringify(response));
         }
 
