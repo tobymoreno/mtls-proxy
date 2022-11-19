f3a741a93972e51319aa0e798356707664469d30
toby moreno <tobymoreno@Karols-Air.home>
2022-11-19 14:33:44 -0800

    fixing test.sh for docker login

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,12 +5,14 @@ IMAGE_CHECK="Image is up to date"
 while [ 1 -ne 0 ]
 do
     date
+    docker login -utoby.moreno@gmail.com devsecopsenv.jfrog.io -p "${jfrog-pwd}"
     docker pull 1rom2ny/mtls-proxy:v7 | grep "${IMAGE_CHECK}"
 
     if [ $? -ne 1 ]; then 
         echo "${IMAGE_CHECK}"
     else
         echo "There is a new image. Update the service"
+        kubectl apply -f ../secrets.yml
         kubectl replace -f ../deploymentservice-cluster.yml
         sleep 2
         kubectl apply -f ../mtls-loadbalancer.yaml
